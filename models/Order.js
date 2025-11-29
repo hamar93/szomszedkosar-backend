140b298d400d4e307f9f8eca702efd988565419e
hamar93 <hamargabor1993@gmail.com>
2025-11-29 10:36:28 +0100

    feat: create Mongoose Order model

--- models/Order.js
+++ models/Order.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
     buyerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        type: String, // String to support email addresses or user IDs
         required: true
     },
     productId: {
@@ -11,25 +10,8 @@ const OrderSchema = new mongoose.Schema({
         ref: 'Product',
         required: true
     },
-    sellerId: { // Denormalized for easier querying
-        type: String, // Storing email or ID depending on how we reference sellers. Product has sellerName, but maybe we should store sellerEmail or ID.
-        // Looking at Product.js, it has sellerName. Let's look at how we identify sellers.
-        // Product.js doesn't seem to have sellerId, just sellerName. Wait.
-        // Let's check Product.js again.
-        // It has sellerName.
-        // In routes/products.js we filter by sellerEmail.
-        // We should probably store sellerEmail in Product too if it's not there.
-        // Let's check if Product has sellerEmail.
-        // I viewed Product.js and it DOES NOT have sellerEmail.
-        // But routes/products.js filters by sellerEmail.
-        // Let's check routes/products.js again.
-        // "const { sellerEmail } = req.query; ... query.sellerEmail = sellerEmail;"
-        // This implies Product model MUST have sellerEmail.
-        // I must have missed it or it's missing in the file I viewed.
-        // Let me re-read the view_file output for Product.js.
-        // It has: name, price, unit, description, sellerName, location, image, category, isUrgent, createdAt.
-        // It DOES NOT have sellerEmail.
-        // This is a bug in the backend. I need to add sellerEmail to Product.js as well.
+    sellerId: {
+        type: String, // Seller email or ID
     },
     quantity: {
         type: Number,
